5c3df3976a259fa193b0c56583942cadffaf27d5
doraro <475792096@qq.com>
2019-03-12 12:03:54 +0800

    java8日期序列化支持

--- src/main/java/com/doraro/permission_fast/util/Constant.java
+++ src/main/java/com/doraro/permission_fast/util/Constant.java
@@ -8,4 +8,10 @@ public interface Constant {
     String SUPER_ROLE_NAME = "admin";
     String TOKEN_PREFIX = "Bearer ";
     String TOKEN_KEY = "Authorization";
+    /** 默认日期时间格式 */
+    String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    /** 默认日期格式 */
+    String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    /** 默认时间格式 */
+    String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 }
